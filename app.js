484b32a03fa4b9fcf9ad993806426318d4fa4198
CharlotteDu <31658424+CharlotteDu@users.noreply.github.com>
2017-10-24 16:06:45 +0200

    Etape 2 avec logo

--- app.js
+++ app.js
@@ -13,18 +13,21 @@ var app = new Vue({
       dateEnd: "Today",
       name: "Agence France Presse",
       title: "Alternante",
-      description: "Passée par les services Desk france, Infrographie, Société, Réseaux Sociaux et la rubrique cinéma"
+      logo: "https://www.afp.com/sites/all/themes/custom/afpcom/logo.png",
+      description: "Passée par les services Desk france, Infrographie, Société, Réseaux Sociaux et la rubrique cinéma",
     }, {
       dateBegin: "Été 2016",
       dateEnd: false,
       name: "Le Relève et la Peste",
       title: "Journaliste stagiaire",
+      logo: "https://pbs.twimg.com/profile_images/785417519377031168/LIUJdFMe.jpg",
       description: false
     }, {
       dateBegin: "Eté 2014",
       dateEnd: false,
       name: "La Croix",
       title: "Journaliste Stagiaire",
+      logo: "https://www.la-croix.com/vb903611/bundles/lacroixsite/images/logo.svg",
       description: false
     }],
     formations: [{
@@ -37,6 +40,18 @@ var app = new Vue({
       dateEnd: "2016",
       name: "Master d'Etudes Théâtrales",
       university: "Ecole Normale Supérieure de Lyon"
+    },
+    {
+      dateBegin: "2011",
+      dateEnd: "2013",
+      name: "Hypokhâgne / Khâgne",
+      university: "Lycée Fénelon"
+    },
+    {
+      dateBegin: "2011",
+      dateEnd: false,
+      name: "Baccalauréat Scientifique Mention Très Bien",
+      university: "Lycée Richelieu"
     }],
     languages: ["Anglais", "Russe", "Japonais"],
     skills: ["Première Pro", "Audition", "VueJS", "SCSS", "HTML"]
